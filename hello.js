28f71851af8b876d941cec27436125d2ee36cd74
James Ward <jimmywardmichael@gmail.com>
2023-11-06 10:13:59 -0600

    javascript lesson 1

--- hello.js
+++ hello.js
@@ -1,1 +1,15 @@
-console.log("hello");
+console.log('This is a single backslash character: "\\"');
+console.log("Here's a number: \"two\"");
+console.log(typeof(99.9));
+console.log(typeof("false"));
+console.log(typeof(false));
+console.log('0');
+console.log(0);
+console.log(true);
+console.log('true');
+console.log('1' + 2);
+console.log(typeof('1' + 2));
+console.log(5 % 2);
+console.log(5 % 2);
+console.log(5 % 2);
+
